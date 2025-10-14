c31aee024dd5eef559b90a1cafc490ff602b4530
retkesbegre <115552479+retkesbegre@users.noreply.github.com>
2025-10-14 13:23:32 +0200

    frontend_ujrakezdes

--- fogado/frontend/src/App.jsx
+++ fogado/frontend/src/App.jsx
@@ -2,25 +2,6 @@ function App() {
   const [selectedRoom, setSelectedRoom] = useState("");
 
   return (
-    <div className="container">
-      <h1 className="text-center my-4">Szobafoglaltság lekérdezés</h1>
-
-      <div className="mb-4 text-center">
-        <label htmlFor="roomSelect" className="form-label">Válassz szobát:</label>
-        <select
-          id="roomSelect"
-          className="form-select w-auto mx-auto"
-          value={selectedRoom}
-          onChange={(e) => setSelectedRoom(e.target.value)}
-        >
-          <option value="">-- Válassz --</option>
-          <option value="101">101</option>
-          <option value="102">102</option>
-          <option value="103">103</option>
-        </select>
-      </div>
-
-      <TablazatSzoba selectedRoom={selectedRoom} />
-    </div>
+    <div><img src="" alt="" /></div>
   );
 }
